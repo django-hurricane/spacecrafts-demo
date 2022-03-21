cf75df92e7b7d0d0ac238753faf4f2d1961a62c5
Robert Gutschale <r.gutschale@blueshoe.de>
2022-03-21 15:24:52 +0100

    feat: add custom check

--- src/apps/components/apps.py
+++ src/apps/components/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ComponentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.components"
+
+    def ready(self):
+        from django.core.checks import register
+
+        from apps.components.checks import example_check_main_engine
+
+        register(example_check_main_engine, "hurricane")
---
+++ src/apps/components/checks.py
@@ -0,0 +1,29 @@
+import logging
+
+from django.core.checks import Error
+
+from apps.components.models import Component
+
+logger = logging.getLogger("hurricane")
+
+
+def example_check_main_engine(app_configs=None, **kwargs):
+    """
+    Check for existence of the "Main engine" component in the database
+    """
+
+    # your check logic here
+    errors = []
+    logger.info("Our check has been called :]")
+    # we need to wrap all sync calls to the database into a sync_to_async wrapper for hurricane to use it in async way
+    if not Component.objects.filter(title="Main engine").exists():
+        errors.append(
+            Error(
+                "an error",
+                hint="There is no main engine in the spacecraft, it need's to exist with the name 'Main engine'. "
+                "Please create it in the admin or by installing the fixture.",
+                id="components.E001",
+            )
+        )
+
+    return errors
